e010d5c542e25952aa5cf045947107575ada03c7
Gayatri-Bharne <gpb5496@gmail.com>
2022-01-03 12:14:42 +0530

    Done

---
+++ WelcomePage/WelcomePage/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WelcomePage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        { 
+         
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult Products()
+        {
+            return View();
+        }
+        public IActionResult Clients()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ WelcomePage/WelcomePage/Models/SendMailDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WelcomePage.Models
+{
+    public class SendMailDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public string ContactNo { get; set; }
+        public string Message { get; set; } 
+       
+    }
+}
